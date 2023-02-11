5a57e6bd5f9e590503a10eecab30e983d8232734
kulotsystems <kulotsystems@gmail.com>
2023-02-11 17:24:47 +0800

    Isolate database config.

--- app/index.php
+++ app/index.php
@@ -1,10 +1,10 @@
 <?php
-session_start();
 header('Access-Control-Allow-Origin: http://localhost:5175');
 header("Access-Control-Allow-Credentials: true");
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Access-Control-Allow-Headers: Content-Type');
-require_once 'config/env.php';
+session_start();
+require_once 'config/database.php';
 
 if(isset($_GET['getUser']))
 {
