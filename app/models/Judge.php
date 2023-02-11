4c667d2a96605265d2110d5553b442307464afcb
kulotsystems <kulotsystems@gmail.com>
2023-02-11 09:17:55 +0800

    Create table attribute.

--- app/models/Judge.php
+++ app/models/Judge.php
@@ -4,8 +4,11 @@ require_once 'User.php';
 
 class Judge extends User
 {
+    protected $table = 'users_judge';
+
+
     public function __construct($username, $password)
     {
-        parent::__construct($username, $password, 'users_judge', 'judge');
+        parent::__construct($username, $password, $this->table, 'judge');
     }
 }
